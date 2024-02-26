b9fffbe4c537edf7e97cb2cf5fec58cdb62e73aa
Mateus Arce <arcemateuss@gmail.com>
2024-02-26 15:30:23 -0400

    criando alguns testes

--- test/categories.spec.ts
+++ test/categories.spec.ts
@@ -1,21 +1,32 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async() => {
-  await app.ready()
+describe('Categories routes', () => {
+
+  beforeAll(async() => {
+    await app.ready()
+  })
+  
+  afterAll(async () => {
+    await app.close()
+  })
+  
+  it('should be able to create a new category', async () => {
+    await request(app.server)
+      .post('/categories')
+      .send({
+        "name": "Teste",
+        "description": "Perguntas e respostas relacionadas à testes de software"
+      })
+      .expect(201)
+  }) 
+
+  it('should be able to list all categories', async() => {
+    const listCategoriesResponse = await request(app.server)
+    .get('/categories')
+    .expect(200)
+  })
+
 })
 
-afterAll(async () => {
-  await app.close()
-})
-
-test('user can create a new category', async () => {
-  await request(app.server)
-    .post('/categories')
-    .send({
-      "name": "Teste",
-      "description": "Perguntas e respostas relacionadas à testes de software"
-    })
-    .expect(201)
-}) 
---
+++ test/questions.spec.ts
@@ -0,0 +1,25 @@
+import { it, beforeAll, afterAll, describe } from 'vitest'
+import request  from 'supertest'
+import { app } from '../src/app'
+
+describe('Questions routes', () => {
+
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new question', async () => {
+    await request(app.server)
+      .post('/questions')
+      .send({
+        "title": "O que é PHP?",
+        "category_id": "5f10734d-8b48-4252-bf68-44a5cc473709"
+      })
+      .expect(201)
+  })
+
+})
